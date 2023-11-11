d9ac40c75cad6fdd934d53993fd4982cd24f6c07
Мария <marklukanova@gmail.com>
2023-11-11 17:33:55 +0300

    feat: add map component

--- src/const.ts
+++ src/const.ts
@@ -16,3 +16,14 @@ export enum AuthorizationStatus {
   NoAuth = 'NO_AUTH',
   Unknown = 'UNKNOWN',
 }
+
+export const CITIES_MAP = {
+  Amsterdam: {
+    location: {
+      latitude: 52.3909553943508,
+      longitude: 4.85309666406198,
+      zoom: 10,
+    },
+    name: 'Amsterdam',
+  }
+};
