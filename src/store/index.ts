71e297f35eae619522068f4a6b895ac3379961db
Мария <marklukanova@gmail.com>
2023-11-18 02:46:01 +0300

    fix: remove commented lines

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,4 @@
 import {configureStore} from '@reduxjs/toolkit';
 import { reducer } from './reducer';
 
-// Инициализируйте (например, в файле src/store/index.ts) новое хранилище
-// с помощью функции configureStore из пакета @reduxjs/toolkit.
-// Оберните основной компонент приложения (App) в <Provider> из пакета react-redux.
-// Через props передайте ему ссылку на созданное хранилище.
-
-
-// import reducer
-
 export const store = configureStore({reducer});
