e7cdc68c101db6f15673fa3ec5e3e25f9d41ff8e
Мария <marklukanova@gmail.com>
2023-11-20 18:33:31 +0300

    feat: prepare api and configure thunk

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,8 +1,11 @@
 import { createAction } from '@reduxjs/toolkit';
 import { OfferType } from '../types/offer-type';
+import { AuthorizationStatus } from '../const';
 
 export const changeCity = createAction<string>('changeCity');
 
 export const getOffers = createAction<OfferType[]>('getOffers');
 
 export const changeSortOption = createAction<string>('changeSorting');
+
+export const requireAuthorization = createAction<AuthorizationStatus>('reqiireAuth');
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -4,17 +4,20 @@ import { OfferType } from '../types/offer-type';
 import { CardOffer } from '../mocks/cardOffer';
 import { changeCity, changeSortOption, getOffers } from './action.ts';
 import { DEFAULT_CITY } from '../const.ts';
+import { AuthorizationStatus } from '../const.ts';
 
 type StateType = {
   city: string;
   offers: OfferType[];
   sortingOption: string;
+  authorizationStatus: AuthorizationStatus;
 };
 
 const InitialState : StateType = {
   city: DEFAULT_CITY,
   offers: CardOffer,
   sortingOption: 'Popular',
+  authorizationStatus: AuthorizationStatus.Unknown,
 };
 
 export const reducer = createReducer(InitialState, (builder) => {
